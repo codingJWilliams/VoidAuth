90d25678d12c9058e1138852c98e0d966a896ab6
Jay Williams <course@jaywilliams.me>
2017-12-27 21:24:57 +0000

    Adding WIP discord auth

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var config = require("./config.json");
 const fs = require("fs");
 const crypto = require('crypto');
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const phonetic = require("phonetic");
+
+app.set('view engine', 'ejs');
+
 // Prints:
 app.use(express.static("assets"))
 app.use(session({
@@ -28,7 +33,14 @@ app.get("/haveusername", (req, res) => {
   var usernamefile = JSON.parse(fs.readFileSync("./users.json"));
   if (!usernamefile.hasOwnProperty(req.query.username)) return res.sendFile(__dirname + "/assets/incorrectusername.html")
   req.session.username = req.query.username;
-  res.sendFile(__dirname + "/assets/passwordAuth.html")
+  if (usernamefile[req.session.username].type === "standard") {
+    res.sendFile(__dirname + "/assets/passwordAuth.html")
+  } else if (usernamefile[req.session.username].type == "discord") {
+    var code = phonetic.generate().toLowerCase();
+    axios.get("http://jwte.ch:8000/2fa/" + usernamefile[req.session.username].did + "/" + code);
+    req.session.code = code
+    res.sendFile(__dirname + "/assets/discordAuth.html");
+  }
 })
 app.get("/havepassword", (req, res) => {
   if (!req.session.redirect || req.session.username == undefined || !req.query.password) return res.end("401: Malformed request");
@@ -42,6 +54,21 @@ app.get("/havepassword", (req, res) => {
   user.username = req.session.username
   user.logintime = Date.now();
   delete user.password
+  var token = jwt.sign(user, privatekey, {
+    algorithm: "RS256",
+    issuer: "JW-Centro"
+  })
+  res.redirect(req.session.redirect.replace("$token", token))
+})
+app.get("/havediscord", (req, res) => {
+  if (!req.session.redirect || req.session.username == undefined || !req.query.discord) return res.end("401: Malformed request");
+  var usernamefile = JSON.parse(fs.readFileSync("./users.json"));
+
+  if (req.session.code !== req.query.discord) return res.sendFile(__dirname + "/assets/incorrectdiscord.html");
+  var privatekey = fs.readFileSync("private.key");
+  var user = usernamefile[req.session.username];
+  user.username = req.session.username
+  user.logintime = Date.now();
   var token = jwt.sign(user, privatekey, {
     algorithm: "RS256",
     issuer: "JW-Centro"
